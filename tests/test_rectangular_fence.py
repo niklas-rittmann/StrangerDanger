465a045ab7c6ef4fe22e53d10318ef0d673649ed
Niklas Rittmann <niklas.rittmann@me.com>
2022-02-19 13:34:30 +0100

    test: Added tests for two fences

---
+++ tests/test_rectangular_fence.py
@@ -0,0 +1,41 @@
+from _pytest.recwarn import warns
+import pytest
+from pydantic import ValidationError
+from stranger_danger.fences.rectangular_fence import RectangularFence
+from stranger_danger.fences.protocol import Coordinate, Fence
+
+
+@pytest.fixture
+def rec() -> Fence:
+    return RectangularFence(
+        name="Test Fence", coordinates=(Coordinate(x=2, y=3), Coordinate(x=0, y=0))
+    )
+
+
+def test_validation_error():
+    """Test whether the input validation works"""
+
+    with pytest.raises(ValidationError):
+        RectangularFence(center=4, radius=1)
+
+    with pytest.raises(ValidationError):
+        RectangularFence(center=(3, 3), radius=(1,))
+
+
+def test_attributes(rec):
+    """Test if argument assinment works"""
+    assert rec.name == "Test Fence"
+    assert rec.coordinates[0].x == 2
+    assert rec.coordinates[0].y == 3
+    assert rec.coordinates[1].x == 0
+    assert rec.coordinates[1].y == 0
+
+
+def test_inside_func(rec):
+    """Test if a point inside the recle is recognised"""
+    assert rec.inside_fence(Coordinate(x=1, y=1))
+
+
+def test_outside_func(rec):
+    """Test if a point outside the recle is recognised"""
+    assert not rec.inside_fence(Coordinate(x=3, y=4))
