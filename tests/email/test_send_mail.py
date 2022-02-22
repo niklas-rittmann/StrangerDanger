4f6a5d7edfda7e24d7c8a18422180ab188eb7275
Niklas Rittmann <niklas.rittmann@me.com>
2022-02-22 21:57:55 +0100

    chore: Set default to not empty string

--- tests/email/test_send_mail.py
+++ tests/email/test_send_mail.py
@@ -11,7 +11,7 @@ def test_connection_check(monkeypatch):
     assert not Email(server="", receivers=["test@tests.de"]).check_connection()
 
     monkeypatch.setattr(Email, "_establish_connection", lambda x, y: True)
-    assert Email(server="", receivers=["test@tests.de"]).check_connection()
+    assert Email(server="test_server", receivers=["test@tests.de"]).check_connection()
 
 
 def test_create_message():
