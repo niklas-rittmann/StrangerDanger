f11c85521ddeacf3e7cbd0ebe5c69e9448c90665
Niklas Rittmann <niklas.rittmann@me.com>
2022-02-23 20:02:19 +0100

    chore: Add property to coordinate and add draw fence to protocol

--- stranger_danger/fences/protocol.py
+++ stranger_danger/fences/protocol.py
@@ -1,16 +1,28 @@
-from typing import Protocol
+from typing import Protocol, Tuple
 
+import numpy as np
 from pydantic.main import BaseModel
+
+Image = np.ndarray
 
 
 class Coordinate(BaseModel):
     x: int
     y: int
 
+    @property
+    def tuple(self) -> Tuple[int, int]:
+        """Return tuple representation of coordinate"""
+        return (self.x, self.y)
+
 
 class Fence(Protocol):
 
     name: str
+
+    async def draw_fence(self) -> Image:
+        """Draw the fence into an empty image"""
+        ...
 
     async def inside_fence(self, point: Coordinate) -> bool:
         """Check if given coordinate in Fence"""
