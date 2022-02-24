ac3ab6aedf1b0fb7e25b6662743efd0ec27d8775
Niklas Rittmann <niklas.rittmann@me.com>
2022-02-24 22:27:44 +0100

    feature: Add database representation of objects

---
+++ stranger_danger/db/session.py
@@ -0,0 +1,16 @@
+from typing import AsyncIterable, Iterable
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from stranger_danger.db.config.settings import DB_URL
+
+engine = create_async_engine(DB_URL, echo=True)
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+
+async def create_session() -> AsyncIterable[AsyncSession]:
+    """Yield a databse session"""
+    async with async_session() as session:
+        yield session
+        await session.commit()
