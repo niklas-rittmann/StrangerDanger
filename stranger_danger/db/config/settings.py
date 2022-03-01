ef186e5c766ff2a217199fcc81b8d9b9706b4cfa
Niklas Rittmann <niklas.rittmann@me.com>
2022-03-01 20:29:46 +0100

    chore: Move declrative base to settings

--- stranger_danger/db/config/settings.py
+++ stranger_danger/db/config/settings.py
@@ -4,5 +4,6 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_DATABASE = os.getenv("DB_DATABASE")
+DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}"
 
-DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}"
+Base = declarative_base()
