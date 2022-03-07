1bfa6cde66e072aa44f9cd05560f948c65df1e15
Niklas Rittmann <niklas.rittmann@me.com>
2022-03-07 22:03:46 +0100

    tests: Add test for load and resize image

--- tests/detector/test_file_observer.py
+++ tests/detector/test_file_observer.py
@@ -1,7 +1,19 @@
 import time
 
+import cv2
+import numpy as np
+
+from stranger_danger.constants.image_constants import H, W
 from stranger_danger.detector import event_listener as ev_l
 from stranger_danger.detector.detector import Detector
+
+
+def test_resize_image(monkeypatch):
+    """Test the resizing of images"""
+    monkeypatch.setattr(cv2, "imread", lambda _: np.zeros((1000, 1000, 3)))
+    image = ev_l.load_resize_image("bla")
+    assert isinstance(image, np.ndarray)
+    assert image.shape == (H, W, 3)
 
 
 def test_start_stop(detector: Detector, monkeypatch):
