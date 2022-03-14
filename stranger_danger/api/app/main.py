abbbb9d1d77dfb3e86c4502e660e7cb6d4c10721
Niklas Rittmann <niklas.rittmann@me.com>
2022-03-14 20:41:20 +0100

    feature: add main api endpoint

---
+++ stranger_danger/api/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from stranger_danger.api.app.routers import circular_fences, rectangular_fences
+
+app = FastAPI()
+
+
+app.include_router(rectangular_fences.router)
+app.include_router(circular_fences.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Applications!"}
