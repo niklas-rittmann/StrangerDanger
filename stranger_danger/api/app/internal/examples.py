f8711097b486493212037a10e3cc91b3ff2a2bfa
Niklas Rittmann <niklas.rittmann@me.com>
2022-03-15 20:12:28 +0100

    feature: Extend the examples to create different fences

--- stranger_danger/api/app/internal/examples.py
+++ stranger_danger/api/app/internal/examples.py
@@ -1,26 +1,35 @@
 from fastapi.param_functions import Body
 
-rec_example = Body(
+fence_examples = Body(
     ...,
-    example={
-        "name": "Rectangular Fence",
-        "coordinates": [
-            {"x": 30, "y": 40},
-            {"x": 70, "y": 80},
-        ],
-    },
-)
-
-pent_example = Body(
-    ...,
-    example={
-        "name": "Rectangular Fence",
-        "coordinates": [
-            {"x": 0, "y": 0},
-            {"x": 30, "y": 0},
-            {"x": 30, "y": 40},
-            {"x": 30, "y": 50},
-            {"x": 30, "y": 40},
-        ],
+    examples={
+        "Rectangular": {
+            "value": {
+                "name": "Rectangular Fence",
+                "coordinates": [
+                    {"x": 0, "y": 0},
+                    {"x": 30, "y": 0},
+                ],
+            },
+        },
+        "Pentagon": {
+            "value": {
+                "name": "Pentagon Fence",
+                "coordinates": [
+                    {"x": 0, "y": 0},
+                    {"x": 30, "y": 0},
+                    {"x": 30, "y": 40},
+                    {"x": 30, "y": 50},
+                    {"x": 30, "y": 40},
+                ],
+            },
+        },
+        "Circular": {
+            "value": {
+                "name": "Circular Fence",
+                "center": {"x": 0, "y": 0},
+                "radius": 0,
+            },
+        },
     },
 )
