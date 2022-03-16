9b8a109c5148e3f2f8096cb2b93a8a242604e06b
Niklas Rittmann <niklas.rittmann@me.com>
2022-03-16 19:16:46 +0100

    feature: Add endpoint for detectors

--- stranger_danger/api/app/main.py
+++ stranger_danger/api/app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
-from stranger_danger.api.app.routers import fences
+from stranger_danger.api.app.routers import detector, fences
 
 app = FastAPI()
 
 
 app.include_router(fences.router)
+app.include_router(detector.router)
 
 
 @app.get("/")
